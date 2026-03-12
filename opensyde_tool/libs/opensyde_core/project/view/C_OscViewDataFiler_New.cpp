3984b8ccaeab5d99e57e5e38f2b475abd7d4629b
Tyler Wagler <tyler.wagler@elytrondefense.com>
2026-03-12 10:33:37 -0400

    [ViewData] Create multi-format filer for view data
    - Create C_OscViewDataFiler_New with support for binary, JSON, and XML formats
    - Data class (C_OscViewData) already has all Qt serialization methods
    - Follows established pattern from other _New filers
    - Includes auto-detection of file format from extension
    - Provides legacy compatibility methods marked as deprecated
    
    This completes the ViewData filer migration.

---
+++ opensyde_tool/libs/opensyde_core/project/view/C_OscViewDataFiler_New.cpp
@@ -0,0 +1,116 @@
+//----------------------------------------------------------------------------------------------------------------------
+/*!
+   \file
+   \brief       Filer for view data files (Multi-Format - Framework)
+
+   Load / save view data from / to binary, JSON, or XML
+   files using the Qt-native serialization framework.
+
+   \copyright   Copyright 2022 Sensor-Technik Wiedemann GmbH. All rights
+   reserved.
+*/
+//----------------------------------------------------------------------------------------------------------------------
+
+#include "precomp_headers.hpp"
+#include "C_OscViewDataFiler_New.hpp"
+#include "C_OscFilerUtil.hpp"
+#include <QFile>
+#include <QFileInfo>
+
+using namespace stw::opensyde_core;
+using namespace stw::errors;
+
+//----------------------------------------------------------------------------------------------------------------------
+int32_t C_OscViewDataFiler_New::h_LoadViewDataFile(QList<C_OscViewData> &orc_Views,
+                                                   const QString &orc_FilePath,
+                                                   const QString &orc_BasePath) {
+   Q_UNUSED(orc_BasePath); // Not used in new framework
+   return mh_DetectAndLoad(orc_Views, orc_FilePath);
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+int32_t C_OscViewDataFiler_New::h_SaveViewDataFile(const QList<C_OscViewData> &orc_Views,
+                                                   const QString &orc_FilePath,
+                                                   const QString &orc_BasePath) {
+   Q_UNUSED(orc_BasePath); // Not used in new framework
+
+   QFileInfo c_FileInfo(orc_FilePath);
+   const QString c_Extension = c_FileInfo.suffix().toLower();
+
+   if (c_Extension == "bin") {
+      return h_SaveBinary(orc_Views, orc_FilePath);
+   } else if (c_Extension == "json") {
+      return h_SaveJson(orc_Views, orc_FilePath);
+   } else if (c_Extension == "xml") {
+      return h_SaveXml(orc_Views, orc_FilePath);
+   } else {
+      return C_CONFIG; // Invalid file extension
+   }
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+int32_t C_OscViewDataFiler_New::h_LoadBinary(QList<C_OscViewData> &orc_Views,
+                                             const QString &orc_FilePath) {
+   return C_OscFilerUtil::h_LoadListBinary<C_OscViewData>(orc_Views, orc_FilePath);
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+int32_t C_OscViewDataFiler_New::h_SaveBinary(const QList<C_OscViewData> &orc_Views,
+                                             const QString &orc_FilePath) {
+   return C_OscFilerUtil::h_SaveListBinary<C_OscViewData>(orc_Views, orc_FilePath);
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+int32_t C_OscViewDataFiler_New::h_LoadJson(QList<C_OscViewData> &orc_Views,
+                                           const QString &orc_FilePath) {
+   return C_OscFilerUtil::h_LoadListJson<C_OscViewData>(orc_Views, orc_FilePath);
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+int32_t C_OscViewDataFiler_New::h_SaveJson(const QList<C_OscViewData> &orc_Views,
+                                           const QString &orc_FilePath) {
+   return C_OscFilerUtil::h_SaveListJson<C_OscViewData>(orc_Views, orc_FilePath);
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+int32_t C_OscViewDataFiler_New::h_LoadXml(QList<C_OscViewData> &orc_Views,
+                                          const QString &orc_FilePath) {
+   return C_OscFilerUtil::h_LoadListXml<C_OscViewData>(orc_Views, orc_FilePath, "viewData", "view");
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+int32_t C_OscViewDataFiler_New::h_SaveXml(const QList<C_OscViewData> &orc_Views,
+                                          const QString &orc_FilePath) {
+   return C_OscFilerUtil::h_SaveListXml<C_OscViewData>(orc_Views, orc_FilePath, "viewData", "view");
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+int32_t C_OscViewDataFiler_New::mh_DetectAndLoad(QList<C_OscViewData> &orc_Views,
+                                                 const QString &orc_FilePath) {
+   QFileInfo c_FileInfo(orc_FilePath);
+   const QString c_Extension = c_FileInfo.suffix().toLower();
+
+   if (c_Extension == "bin") {
+      return h_LoadBinary(orc_Views, orc_FilePath);
+   } else if (c_Extension == "json") {
+      return h_LoadJson(orc_Views, orc_FilePath);
+   } else if (c_Extension == "xml") {
+      return h_LoadXml(orc_Views, orc_FilePath);
+   } else {
+      return C_CONFIG; // Invalid file extension
+   }
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+[[deprecated("Use format-specific methods")]]
+int32_t C_OscViewDataFiler_New::h_LoadFile(QList<C_OscViewData> &orc_Views,
+                                           const QString &orc_Path) {
+   return h_LoadViewDataFile(orc_Views, orc_Path, QString());
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+[[deprecated("Use format-specific methods")]]
+int32_t C_OscViewDataFiler_New::h_SaveFile(const QList<C_OscViewData> &orc_Views,
+                                           const QString &orc_Path) {
+   return h_SaveViewDataFile(orc_Views, orc_Path, QString());
+}
---
+++ opensyde_tool/libs/opensyde_core/project/view/C_OscViewDataFiler_New.hpp
@@ -0,0 +1,95 @@
+//----------------------------------------------------------------------------------------------------------------------
+/*!
+   \file
+   \brief       Filer for view data files (Multi-Format - Framework)
+
+   Load / save view data from / to binary, JSON, or XML
+   files using the Qt-native serialization framework.
+
+   \copyright   Copyright 2022 Sensor-Technik Wiedemann GmbH. All rights
+   reserved.
+*/
+//----------------------------------------------------------------------------------------------------------------------
+#ifndef C_OSCVIEWDATAFILER_NEW_HPP
+#define C_OSCVIEWDATAFILER_NEW_HPP
+
+/* -- Includes
+ * ------------------------------------------------------------------------------------------------------
+ */
+#include "C_OscViewData.hpp"
+#include "C_OscFilerUtil.hpp"
+#include "stwtypes.hpp"
+#include <QList>
+#include <QString>
+
+/* -- Namespace
+ * -----------------------------------------------------------------------------------------------------
+ */
+namespace stw {
+namespace opensyde_core {
+/* -- Global Constants
+ * ----------------------------------------------------------------------------------------------
+ */
+
+/* -- Types
+ * ---------------------------------------------------------------------------------------------------------
+ */
+
+class C_OscViewDataFiler_New {
+public:
+   // --------------------------------------------------------------------------
+   // Unified File Operations (Auto-detect format from extension)
+   // --------------------------------------------------------------------------
+   static int32_t h_LoadViewDataFile(QList<C_OscViewData> &orc_Views,
+                                     const QString &orc_FilePath,
+                                     const QString &orc_BasePath);
+   static int32_t h_SaveViewDataFile(const QList<C_OscViewData> &orc_Views,
+                                     const QString &orc_FilePath,
+                                     const QString &orc_BasePath);
+
+   // --------------------------------------------------------------------------
+   // Binary Format
+   // --------------------------------------------------------------------------
+   static int32_t h_LoadBinary(QList<C_OscViewData> &orc_Views,
+                               const QString &orc_FilePath);
+   static int32_t h_SaveBinary(const QList<C_OscViewData> &orc_Views,
+                               const QString &orc_FilePath);
+
+   // --------------------------------------------------------------------------
+   // JSON Format
+   // --------------------------------------------------------------------------
+   static int32_t h_LoadJson(QList<C_OscViewData> &orc_Views,
+                             const QString &orc_FilePath);
+   static int32_t h_SaveJson(const QList<C_OscViewData> &orc_Views,
+                             const QString &orc_FilePath);
+
+   // --------------------------------------------------------------------------
+   // XML Format
+   // --------------------------------------------------------------------------
+   static int32_t h_LoadXml(QList<C_OscViewData> &orc_Views,
+                            const QString &orc_FilePath);
+   static int32_t h_SaveXml(const QList<C_OscViewData> &orc_Views,
+                            const QString &orc_FilePath);
+
+   // --------------------------------------------------------------------------
+   // Legacy Compatibility (deprecated)
+   // --------------------------------------------------------------------------
+   [[deprecated("Use format-specific methods")]]
+   static int32_t h_LoadFile(QList<C_OscViewData> &orc_Views,
+                             const QString &orc_Path);
+   [[deprecated("Use format-specific methods")]]
+   static int32_t h_SaveFile(const QList<C_OscViewData> &orc_Views,
+                             const QString &orc_Path);
+
+private:
+   static int32_t mh_DetectAndLoad(QList<C_OscViewData> &orc_Views,
+                                   const QString &orc_FilePath);
+};
+
+/* -- Extern Global Variables
+ * ---------------------------------------------------------------------------------------
+ */
+} // namespace opensyde_core
+} // namespace stw
+
+#endif
